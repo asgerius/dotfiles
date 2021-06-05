eafb0f559d9ea77a1a8dcb1c529dcf3e470608df
Asger Schultz <asger.s@protonmail.com>
2021-06-05 12:05:42 +0200

    Alias

--- mjsetup.sh
+++ mjsetup.sh
@@ -7,6 +7,13 @@
 SCRIPTPATH=~/environment-setup
 
 git config --global credential.helper store
+git config --global alias.fukd "push -f"
+echo """
+[alias]
+lg1 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all
+lg2 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all
+lg = !"git lg1"
+""" >> ~/.gitconfig
 
 # Installerer zsh
 yes y | sudo pacman -S zsh
